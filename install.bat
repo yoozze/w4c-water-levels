d9272750f73d94aa0026d79040401749dfe5f8ea
yoozze <ezzooy@gmail.com>
2019-11-05 16:28:30 +0100

    Added Windows installation script

---
+++ install.bat
@@ -0,0 +1,38 @@
+@echo off
+
+echo.
+echo ******************************
+echo * Createing new environment: *
+echo ******************************
+echo.
+call conda create -n w4c
+call conda activate w4c
+
+echo.
+echo *******************************************
+echo * Installing jupyter notebook extensions: *
+echo *******************************************
+echo.
+call conda install nb_conda
+
+echo.
+echo ******************************
+echo * Installing other packages: *
+echo ******************************
+echo.
+call pip install ipywidgets scikit-multiflow gmaps asyncio aiohttp
+
+echo.
+echo *****************************
+echo * Enabling gmaps extension: *
+echo *****************************
+echo.
+call jupyter nbextension enable --py gmaps
+
+echo.
+echo ******************************
+echo * Starting Jupyter notebook: *
+echo ******************************
+echo.
+call jupyter notebook
+
