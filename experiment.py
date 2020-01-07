35f1bbad6a987856606c3c19a55eae6fdbe79d19
yoozze <ezzooy@gmail.com>
2020-01-07 17:00:08 +0100

    Moved GeneticSelector to external library

--- experiment.py
+++ experiment.py
@@ -69,6 +69,7 @@ from skmultiflow.trees import RegressionHoeffdingTree, RegressionHAT, HoeffdingT
 
 from lib.bagging import BaggingRegression
 from sklearn.model_selection import RandomizedSearchCV
+from lib.genetic_selector import GeneticSelector
 
 # %% [markdown]
 # ### 0.3. Settings
@@ -642,116 +643,9 @@ def cluster_df(df, k=0.5):
     df = df.reindex(columns, axis=1)
     return df
 
-# %% [markdown]
-# ### 0.5. Features selection: genetic selector
-
-# %%
-class GeneticSelector():
-    def __init__(self, estimator, n_gen, size, n_best, n_rand, 
-                 n_children, mutation_rate):
-        # Estimator 
-        self.estimator = estimator
-        # Number of generations
-        self.n_gen = n_gen
-        # Number of chromosomes in population
-        self.size = size
-        # Number of best chromosomes to select
-        self.n_best = n_best
-        # Number of random chromosomes to select
-        self.n_rand = n_rand
-        # Number of children created during crossover
-        self.n_children = n_children
-        # Probablity of chromosome mutation
-        self.mutation_rate = mutation_rate
-        
-        if int((self.n_best + self.n_rand) / 2) * self.n_children != self.size:
-            raise ValueError("The population size is not stable.")  
-            
-    def initilize(self):
-        population = []
-        for i in range(self.size):
-            chromosome = np.ones(self.n_features, dtype=np.bool)
-            mask = np.random.rand(len(chromosome)) < 0.3
-            chromosome[mask] = False
-            population.append(chromosome)
-        return population
-
-    def fitness(self, population):
-        X, y = self.dataset
-        scores = []
-        for chromosome in population:
-            score = -1.0 * np.mean(cross_val_score(self.estimator, X[:,chromosome], y, 
-                                                       cv=5, 
-                                                       scoring="neg_mean_absolute_error"))
-            scores.append(score)
-        scores, population = np.array(scores), np.array(population) 
-        inds = np.argsort(scores)
-        return list(scores[inds]), list(population[inds,:])
-
-    def select(self, population_sorted):
-        population_next = []
-        for i in range(self.n_best):
-            population_next.append(population_sorted[i])
-        for i in range(self.n_rand):
-            population_next.append(random.choice(population_sorted))
-        random.shuffle(population_next)
-        return population_next
-
-    def crossover(self, population):
-        population_next = []
-        for i in range(int(len(population)/2)):
-            for j in range(self.n_children):
-                chromosome1, chromosome2 = population[i], population[len(population)-1-i]
-                child = chromosome1
-                mask = np.random.rand(len(child)) > 0.5
-                child[mask] = chromosome2[mask]
-                population_next.append(child)
-        return population_next
-
-    def mutate(self, population):
-        population_next = []
-        for i in range(len(population)):
-            chromosome = population[i]
-            if random.random() < self.mutation_rate:
-                mask = np.random.rand(len(chromosome)) < 0.05
-                chromosome[mask] = False
-            population_next.append(chromosome)
-        return population_next
-
-    def generate(self, population):
-        # Selection, crossover and mutation
-        scores_sorted, population_sorted = self.fitness(population)
-        population = self.select(population_sorted)
-        population = self.crossover(population)
-        population = self.mutate(population)
-        # History
-        self.chromosomes_best.append(population_sorted[0])
-        self.scores_best.append(scores_sorted[0])
-        self.scores_avg.append(np.mean(scores_sorted))
-        
-        return population
-
-    def fit(self, X, y):
- 
-        self.chromosomes_best = []
-        self.scores_best, self.scores_avg  = [], []
-        
-        self.dataset = X, y
-        self.n_features = X.shape[1]
-        
-        population = self.initilize()
-        for i in range(self.n_gen):
-            population = self.generate(population)
-            
-        return self 
-    
-    @property
-    def support_(self):
-        return self.chromosomes_best[-1]
-
 
 # %% [markdown]
-# ### 0.6. Initialization
+# ### 0.5. Initialization
 
 # %%
 pd.set_option('display.max_rows', 5000)
@@ -1318,7 +1212,7 @@ for station_id, dataset in datasets.items():
                         t = time.time()
                         
                         if(len(grid.keys())>0):
-                            gs = RandomizedSearchCV(estimator=m, param_distributions=grid, n_iter=50, cv=3, verbose=2, random_state=42, n_jobs=1)
+                            gs = RandomizedSearchCV(estimator=m, param_distributions=grid, n_iter=1, cv=3, verbose=2, random_state=42, n_jobs=1)
                             gs.fit(X_train, y_train)
                             m = method['class'](**gs.best_params_)
 
